22ce6ce708136fb5c24ca92ce5225db3c92cabcf
Steven Steiner <steets250@gmail.com>
2020-02-23 13:57:59 -0800

    Added Scene Nagivation and Demo

---
+++ Assets/Scripts/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    public GameObject trigger;
+    public Object scene;
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        // Public Variable Checks
+        if (scene == null || scene.GetType().Name != "SceneAsset")
+        {
+            Debug.LogError("Invalid Scene Asset");
+            return;
+        }
+
+        if (trigger == null || trigger.GetType().Name != "GameObject")
+        {
+            Debug.LogError("Invalid Game Object");
+            return;
+        }
+
+        // Switch scene if object name matches
+        if (collision.gameObject.name == trigger.name)
+        {
+            SceneManager.LoadScene(sceneName: scene.name);
+        }
+    }
+}
