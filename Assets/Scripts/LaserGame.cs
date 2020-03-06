871f067e5abc951a01ab9b542de9d058d33dcf3a
Steven Steiner <steets250@gmail.com>
2020-03-06 01:17:23 -0800

    Working Laser List

--- Assets/Scripts/LaserGame.cs
+++ Assets/Scripts/LaserGame.cs
@@ -13,20 +13,20 @@ public class LaserGame : MonoBehaviour
 
     public Material laserMaterial;
 
-    //List<GameObject> hitObjects;
+    List<GameObject> hitObjects;
     List<GameObject> lineContainers;
 
     void Start()
     {
         LineRenderer lineRenderer = startObject.AddComponent<LineRenderer>();
         lineRenderer.material = laserMaterial;
-        //hitObjects = new List<GameObject>();
+        hitObjects = new List<GameObject>();
         lineContainers = new List<GameObject>();
     }
 
     GameObject RunLaser(GameObject inputObject, LineRenderer lineRenderer, Vector3 startDirection)
     {
-        if (inputObject == null || lineRenderer == null)
+        if (inputObject == null || lineRenderer == null || startDirection == null)
         {
             return null;
         }
@@ -46,8 +46,6 @@ public class LaserGame : MonoBehaviour
 
             if (hitObject == inputObject)
             {
-                lineRenderer.positionCount = 1;
-                lineRenderer.SetPosition(0, startObject.transform.position);
                 return null;
             }
 
@@ -72,51 +70,48 @@ public class LaserGame : MonoBehaviour
 
     void Update()
     {
+        hitObjects.Clear();
 
         foreach (GameObject go in lineContainers)
         {
-            Destroy(gameObject);
+            Destroy(go);
         }
 
-        GameObject result = RunLaser(startObject, startObject.GetComponent<LineRenderer>(),Vector3.forward);
+        hitObjects.Add(RunLaser(startObject, startObject.GetComponent<LineRenderer>(), Vector3.forward));
 
-        if (result == splitObject)
+        while (hitObjects.Count != 0)
         {
-            GameObject leftContainer = new GameObject();
-            leftContainer.transform.parent = splitObject.transform;
-            LineRenderer leftRenderer = leftContainer.AddComponent<LineRenderer>();
-            leftRenderer.material = laserMaterial;
-            lineContainers.Add(leftContainer);
-            GameObject leftResult = RunLaser(splitObject, leftRenderer, Vector3.left);
 
-            GameObject rightContainer = new GameObject();
-            rightContainer.transform.parent = splitObject.transform;
-            LineRenderer rightRenderer = rightContainer.AddComponent<LineRenderer>();
-            rightRenderer.material = laserMaterial;
-            lineContainers.Add(rightContainer);
-            GameObject rightResult = RunLaser(splitObject, rightRenderer, Vector3.right);
+            if (hitObjects[0] == splitObject)
+            {
+                GameObject leftContainer = new GameObject();
+                leftContainer.transform.parent = splitObject.transform;
+                LineRenderer leftRenderer = leftContainer.AddComponent<LineRenderer>();
+                leftRenderer.material = laserMaterial;
+                lineContainers.Add(leftContainer);
+                hitObjects.Add(RunLaser(splitObject, leftRenderer, Vector3.left));
 
-            GameObject centerContainer = new GameObject();
-            centerContainer.transform.parent = splitObject.transform;
-            LineRenderer centerRenderer = centerContainer.AddComponent<LineRenderer>();
-            centerRenderer.material = laserMaterial;
-            lineContainers.Add(centerContainer);
-            GameObject centerResult = RunLaser(splitObject, centerRenderer, Vector3.forward);
+                GameObject rightContainer = new GameObject();
+                rightContainer.transform.parent = splitObject.transform;
+                LineRenderer rightRenderer = rightContainer.AddComponent<LineRenderer>();
+                rightRenderer.material = laserMaterial;
+                lineContainers.Add(rightContainer);
+                hitObjects.Add(RunLaser(splitObject, rightRenderer, Vector3.right));
+
+                GameObject centerContainer = new GameObject();
+                centerContainer.transform.parent = splitObject.transform;
+                LineRenderer centerRenderer = centerContainer.AddComponent<LineRenderer>();
+                centerRenderer.material = laserMaterial;
+                lineContainers.Add(centerContainer);
+                hitObjects.Add(RunLaser(splitObject, centerRenderer, Vector3.forward));
+            }
+
+            if (hitObjects[0] == endObject)
+            {
+                endObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+
+            hitObjects.RemoveAt(0);
         }
-
-        //if (hit.collider.gameObject == endObject)
-        //{
-        //    endObject.GetComponent<Renderer>().material.color = Color.green;
-        //    return;
-        //}
-        //else
-        //{
-        //    endObject.GetComponent<Renderer>().material.color = Color.red;
-        //}
-
-        //if (hit.collider.transform.parent == borderObject.transform)
-        //{
-        //    return;
-        //}
     }
 }
