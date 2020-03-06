dddf4b51e71fbdd4a26245c34ca22665f7aacda7
Steven Steiner <steets250@gmail.com>
2020-03-06 00:15:58 -0800

    It Kinda Works

--- Assets/Scripts/LaserGame.cs
+++ Assets/Scripts/LaserGame.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LaserGame : MonoBehaviour
 {
     public GameObject startObject;
+    public GameObject reflectObject;
+    public GameObject splitObject;
     public GameObject endObject;
     public GameObject borderObject;
 
@@ -10,55 +13,110 @@ public class LaserGame : MonoBehaviour
 
     public Material laserMaterial;
 
-    LineRenderer lineRenderer;
+    //List<GameObject> hitObjects;
+    List<GameObject> lineContainers;
 
-    // Start is called before the first frame update
     void Start()
     {
-        lineRenderer = startObject.AddComponent<LineRenderer>();
+        LineRenderer lineRenderer = startObject.AddComponent<LineRenderer>();
         lineRenderer.material = laserMaterial;
+        //hitObjects = new List<GameObject>();
+        lineContainers = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject RunLaser(GameObject inputObject, LineRenderer lineRenderer, Vector3 startDirection)
     {
-        // Begin by rendering point 0 at the starting object
+        if (inputObject == null || lineRenderer == null)
+        {
+            return null;
+        }
+
         lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, startObject.transform.position);
+        lineRenderer.SetPosition(0, inputObject.transform.position);
 
-        // Store the hit and the current laser direction
         RaycastHit hit;
-        Vector3 direction = Vector3.forward;
+        Vector3 direction = startDirection;
 
-        // Loop for every object hit by the laser
         while (Physics.Raycast(lineRenderer.GetPosition(lineRenderer.positionCount - 1), direction, out hit))
         {
-            // Draw new line to the collision point
             lineRenderer.positionCount++;
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
 
-            // Determine next direction to check
-            direction = Vector3.Reflect(direction, hit.normal);
+            GameObject hitObject = hit.collider.gameObject;
 
-            // Check if goal is hit
-            if (hit.collider.gameObject == endObject)
+            if (hitObject == inputObject)
             {
-                endObject.GetComponent<Renderer>().material.color = Color.green;
-                return;
-            }
-            else
-            {
-                endObject.GetComponent<Renderer>().material.color = Color.red;
+                lineRenderer.positionCount = 1;
+                lineRenderer.SetPosition(0, startObject.transform.position);
+                return null;
             }
 
             if (hit.collider.transform.parent == borderObject.transform)
             {
-                return;
+                return borderObject;
             }
+
+            if (hitObject != reflectObject)
+            {
+                return hitObject;
+            }
+
+            direction = Vector3.Reflect(direction, hit.normal);
         }
 
-        // Add tail to the last hit object
         lineRenderer.positionCount++;
         lineRenderer.SetPosition(lineRenderer.positionCount - 1, (lineRenderer.GetPosition(lineRenderer.positionCount - 2) + direction * pointerLength));
+
+        return null;
+    }
+
+    void Update()
+    {
+
+        foreach (GameObject go in lineContainers)
+        {
+            Destroy(gameObject);
+        }
+
+        GameObject result = RunLaser(startObject, startObject.GetComponent<LineRenderer>(),Vector3.forward);
+
+        if (result == splitObject)
+        {
+            GameObject leftContainer = new GameObject();
+            leftContainer.transform.parent = splitObject.transform;
+            LineRenderer leftRenderer = leftContainer.AddComponent<LineRenderer>();
+            leftRenderer.material = laserMaterial;
+            lineContainers.Add(leftContainer);
+            GameObject leftResult = RunLaser(splitObject, leftRenderer, Vector3.left);
+
+            GameObject rightContainer = new GameObject();
+            rightContainer.transform.parent = splitObject.transform;
+            LineRenderer rightRenderer = rightContainer.AddComponent<LineRenderer>();
+            rightRenderer.material = laserMaterial;
+            lineContainers.Add(rightContainer);
+            GameObject rightResult = RunLaser(splitObject, rightRenderer, Vector3.right);
+
+            GameObject centerContainer = new GameObject();
+            centerContainer.transform.parent = splitObject.transform;
+            LineRenderer centerRenderer = centerContainer.AddComponent<LineRenderer>();
+            centerRenderer.material = laserMaterial;
+            lineContainers.Add(centerContainer);
+            GameObject centerResult = RunLaser(splitObject, centerRenderer, Vector3.forward);
+        }
+
+        //if (hit.collider.gameObject == endObject)
+        //{
+        //    endObject.GetComponent<Renderer>().material.color = Color.green;
+        //    return;
+        //}
+        //else
+        //{
+        //    endObject.GetComponent<Renderer>().material.color = Color.red;
+        //}
+
+        //if (hit.collider.transform.parent == borderObject.transform)
+        //{
+        //    return;
+        //}
     }
 }
