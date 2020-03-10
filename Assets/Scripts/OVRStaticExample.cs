d8f90ff1a52d752c42e86460817bcbb600ec0e79
Steven Steiner <steets250@gmail.com>
2020-03-09 21:51:58 -0700

    OVR Gravity Test

---
+++ Assets/Scripts/OVRStaticExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRStaticExample : MonoBehaviour
+{
+    public OVRPlayerController script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
